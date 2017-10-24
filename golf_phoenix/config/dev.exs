c3b324ad7ed1cf7254fe29280dd5a9c7e4445559
RobertDober <robert.dober@gmail.com>
2017-10-24 20:01:52 +0200

    added local secrets to .gitignore

--- golf_phoenix/config/dev.exs
+++ golf_phoenix/config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :golf_phoenix, GolfPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("GOLFTOUT_PG_USER"),
+  password: System.get_env("GOLFTOUR_PG_PASSWORD"), 
   database: "golf_phoenix_dev",
   hostname: "localhost",
   pool_size: 10
